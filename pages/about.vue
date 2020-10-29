0a2c8b5c37f24a6bc3994b2eab2d24a5cda9651a
nicholas.padon <nicholas.padon@oliverwyman.com>
2020-10-29 10:52:33 -0500

    Make about headless CMS driven

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,8 @@
 <template>
-  <v-container>
-    <p class="text-justify">About me</p>
+    <v-container>
+    <v-card>
+      <v-card-text v-html=" $prismic.asHtml(document.main_text)"></v-card-text>
+    </v-card>
   </v-container>
 </template>
 
@@ -8,5 +10,18 @@
 export default {
   name: "About",
   layout: "default",
+  async asyncData({ $prismic, params, error }) {
+    try {
+      // Query to get post content
+      const document = (await $prismic.api.getSingle("about"))
+        .data;
+      return {
+        document,
+      };
+    } catch (e) {
+      // Returns error page
+      error({ statusCode: 404, message: "Page not found" });
+    }
+  },
 };
 </script>
