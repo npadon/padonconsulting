f3697d33f28859f79cdfb10acda83596a9fedfc2
nicholas.padon <nicholas.padon@oliverwyman.com>
2020-10-30 15:59:12 -0500

    Update HTML serializer

--- plugins/html-serializer.js
+++ plugins/html-serializer.js
@@ -29,7 +29,7 @@ export default function(type, element, content, children) {
   // If the image is also a link to a Prismic Document, it will return a <router-link> component
   // Present by default, it is recommended to keep this
   if (type === Elements.image) {
-    let result = `<img src="${element.url}" alt="${element.alt ||
+    let result = `<img width="100%" src="${element.url}" alt="${element.alt ||
       ""}" copyright="${element.copyright || ""}">`;
 
     if (element.linkTo) {
